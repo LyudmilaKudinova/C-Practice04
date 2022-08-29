4ea19f27eaab350fa88579691e87a109be2f3cbd
lyudmila <kudinova@alio-soft.ru>
2022-08-29 20:39:54 +0400

    Вывести массив

---
+++ C#Pr04Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+    
+    int[] array = new int[8];
+    Random myRandom = new Random();
+    Console.Write('[');     
+    for (int i = 0; i < array.Length; i++)
+    
+        {
+            array[i] = myRandom.Next(0, 8);
+           if (i>0) Console.Write(", ");
+            Console.Write("{0} ", array[i]);
+                        
+        }
+    Console.Write(']');
+    Console.WriteLine();
+     
